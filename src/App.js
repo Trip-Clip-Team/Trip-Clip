bd6aebd87ea9e11a54c3269ffb48ebd695d82b9a
JelenaTomic <tomic.elena09@gmail.com>
2022-11-19 15:42:13 -0600

    Added app.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { AuthProvider } from "./Context/auth";
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
+import AuthRoute from "./Utils/authRoute";
+import Map from "./Components/Map";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxhbmhhbm5hIiwiYSI6ImNsYWhqdjc4ajAybTQzcHBoaGw4bTIwcXoifQ.z6Qi0swgA71-QQDo3sobiw';
-
-export default function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-34);
-  const [lat, setLat] = useState(50);
-  const [zoom, setZoom] = useState(2);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
-
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
-
-  return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
-  );
+function App() {
+	return (
+		<div className='app'>
+			<AuthProvider>
+				<Router>
+					<Navbar />
+					<Route exact path='/' component={Home} />
+					<AuthRoute exact path='/login' component={Login} />
+					<AuthRoute exact path='/register' component={Register} />
+					<Route exact path='/map' component={Map} />
+				</Router>
+			</AuthProvider>
+		</div>
+	);
 }
+
+export default App;
